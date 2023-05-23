04da2a8a602185f8b166db8cc9df05a224221c0e
Corey Calbreath <corey_calbreath@yahoo.com>
2023-05-23 00:26:25 -0500

    got html to render

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return (
+    <header>
+        <div class="main-header">
+            <img src="./assets/images/header-image.jpg" alt="a computer icon with an HTML coding icon iside" />
+            <h1>COREY CALBREATH</h1>
+            <h2>Web Developer</h2>
+            </div>
+    </header>
+    );
+}
+
+export default Home;
